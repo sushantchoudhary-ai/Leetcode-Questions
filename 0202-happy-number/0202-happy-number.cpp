1a74a25a4495cfef8bba4156ecde286ecf5f8acf
sushantchoudhary-ai <sushantchoudhary06@gmail.com>
2026-08-13 18:48:28 +0530

    Time: 0 ms (100%), Space: 7.7 MB (83%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+    int sum;
+    while (true) 
+    { 
+        sum = 0; 
+        while (n != 0)
+        { 
+            sum = sum + (n % 10) * (n % 10);
+            n = n / 10; 
+        }
+        
+        n = sum;
+        if (n == 1)
+        { 
+            return true; 
+        }
+        if (n == 4) 
+        {
+            return false;
+        }
+    }
+    }
+    
+};
